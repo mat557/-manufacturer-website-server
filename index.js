ac52f13d8098c41ea5fa817fae34066c2ee88b1f
mat557 <matasnim11@gmail.com>
2022-05-28 13:59:25 +0600

    i am a loser

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:true,
+    optionsSuccessStatus:200,
+    credentials:true
+  }
+));
 app.use(express.json());
 
 
